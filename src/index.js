fd918911b73a921ff7a132d7f28e028dea928b79
Donutboi <43415244+gurilad@users.noreply.github.com>
2022-03-11 15:15:52 +0200

    added fortune and shop ui next is to make the div go away and come back on press of the shop button

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 var clickamt = 0;
 document.getElementById("clicker").onclick = function clicked() {
   document.getElementById("counter").innerHTML = "you have ";
-  clickamt += 1;
+  clickamt += 1 * multiplier;
   document.getElementById("counter").innerHTML += clickamt + " points";
 };
 
@@ -14,5 +14,18 @@ document.getElementById("shop").onclick = function shoptoggle() {
     console.log("test1");
   } else {
     console.log("test");
+  }
+};
+
+var multiplier = 1;
+document.getElementById("fortune").onclick = function fortne() {
+  var cost = multiplier * 20;
+  if (clickamt >= cost) {
+    document.getElementById("counter").innerHTML = "you have ";
+    multiplier *= 2;
+    clickamt -= cost;
+    document.getElementById("counter").innerHTML += clickamt + " points";
+  } else {
+    console.log("not enough points");
   }
 };
